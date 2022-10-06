dad9201c512441e68788740cd17b89719dbc8680
Rodrigo Ribeiro <ribeiro.rodrigodasilva@gmail.com>
2022-10-06 10:05:19 -0300

    [Projeto 04]: Configurando Stitches

--- projects/04-fundamentos-next-ignite-shop/src/pages/index.tsx
+++ projects/04-fundamentos-next-ignite-shop/src/pages/index.tsx
@@ -1,5 +1,14 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat'
+})
+
 export default function Home() {
   return (
+    <>
     <h1>Hello world</h1>
+    <Button>enviar</Button>
+    </>
   )
 }
