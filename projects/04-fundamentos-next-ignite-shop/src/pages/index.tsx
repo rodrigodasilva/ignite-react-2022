9e1024fcb103690377ea72897863a53b891644f4
Rodrigo Ribeiro <ribeiro.rodrigodasilva@gmail.com>
2022-10-07 10:40:26 -0300

    [Projeto 04]: Utilizando SSG

--- projects/04-fundamentos-next-ignite-shop/src/pages/index.tsx
+++ projects/04-fundamentos-next-ignite-shop/src/pages/index.tsx
@@ -1,16 +1,12 @@
 import Image from "next/future/image"
 import { useKeenSlider } from 'keen-slider/react'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
 
 
 import { stripe } from '../lib/stripe'
 
 import { HomeContainer, Product } from "../styles/pages/home"
-
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 
@@ -31,8 +27,6 @@ export default function Home({ products }: HomeProps) {
     }
   });
 
-  console.log(products)
-
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products?.map(product => (
@@ -49,7 +43,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -68,6 +62,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
