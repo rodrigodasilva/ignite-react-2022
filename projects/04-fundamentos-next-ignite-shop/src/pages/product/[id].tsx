6aa5d93284894fab35f7b6823661a9a2c47a4729
Rodrigo Ribeiro <ribeiro.rodrigodasilva@gmail.com>
2022-10-07 16:54:36 -0300

    [Projeto 04]: Checkout no Stripe

--- projects/04-fundamentos-next-ignite-shop/src/pages/product/[id].tsx
+++ projects/04-fundamentos-next-ignite-shop/src/pages/product/[id].tsx
@@ -11,11 +11,16 @@ interface ProductProps {
     name: string
     imageUrl: string
     price: string
-    description: string
+    description: string,
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleBuyButton() {
+    console.log(product.defaultPriceId);
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -26,7 +31,7 @@ export default function Product({ product }: ProductProps) {
         <h1>{product.name}</h1>
         <span>{product.price}</span>
         <p>{product.description}</p>
-        <button>
+        <button onClick={handleBuyButton}>
           Comprar agora
         </button>
       </ProductDetails>
@@ -62,7 +67,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           style: 'currency',
           currency: 'BRL'
         }).format(price.unit_amount / 100),
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1 // 1 hours
